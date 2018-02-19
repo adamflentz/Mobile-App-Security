cc10737478dead39be9b1fa784c6fb2148f65f2d
adamflentz <aflentz@email.wm.edu>
2018-02-19 16:41:14 -0500

    added recipe viewer template

---
+++ app/src/main/java/com/example/adam/recipeapp/RecipeViewer.java
@@ -0,0 +1,13 @@
+package com.example.adam.recipeapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecipeViewer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
