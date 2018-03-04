f5c57abd9c91201f54f1f74486fc07a49bc7d3e2
rcaskari <rcaskari@email.wm.edu>
2018-03-03 20:45:05 -0500

    added button functionallity,with switch statement in case we wanted facebook sharing later and fixed ui bugs

--- app/src/main/java/com/example/adam/recipeapp/MainActivity.java
+++ app/src/main/java/com/example/adam/recipeapp/MainActivity.java
@@ -5,22 +5,44 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button createButton = (Button) findViewById(R.id.createBtn);
+        Button viewButton = (Button) findViewById(R.id.viewBtn);
 
-        Button createButton = (Button) findViewById(R.id.create);
-        createButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Do something in response to button click
-                Intent intent = new Intent(getApplicationContext(), Editor.class);
-                startActivity(intent);
-            }
-        });
+        createButton.setOnClickListener(this);
+        viewButton.setOnClickListener(this);
+
+
+//        createButton.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                // Do something in response to button click
+//                Intent intent = new Intent(getApplicationContext(), Editor.class);
+//                startActivity(intent);
+//            }
+//        });
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.createBtn:
+                Toast.makeText(this, "Create Recipe button clicked", Toast.LENGTH_SHORT).show();
+                Intent createIntent = new Intent(getApplicationContext(), Editor.class);
+                startActivity(createIntent);
+                break;
+            case R.id.viewBtn:
+                Toast.makeText(this, "View Recipes clicked", Toast.LENGTH_SHORT).show();
+                Intent viewIntent = new Intent(getApplicationContext(), RecipeViewer.class);
+                startActivity(viewIntent);
+                break;
+        }
     }
 }
