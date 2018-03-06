e0df783d98d1ebbb882003769f7650c14bfc5cb2
Matthew Swartz <mjswartz92@gmail.com>
2018-03-06 13:36:48 -0500

    Camera functionality

--- app/src/main/java/com/example/adam/recipeapp/CameraPage.java
+++ app/src/main/java/com/example/adam/recipeapp/CameraPage.java
@@ -2,15 +2,20 @@ package com.example.adam.recipeapp;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -18,8 +23,11 @@ import org.json.JSONObject;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -27,6 +35,12 @@ import java.util.List;
  */
 
 public class CameraPage extends AppCompatActivity {
+
+    static final int REQUEST_TAKE_PHOTO = 1;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    String mCurrentPhotoPath;
+    ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,6 +69,18 @@ public class CameraPage extends AppCompatActivity {
         }
 
 
+
+        Button cButton = (Button) findViewById(R.id.takephoto);
+        cButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dispatchTakePictureIntent();
+            }
+        });
+
+
+        mImageView = (ImageView) findViewById(R.id.camera);
+
         Button submitButton = (Button) findViewById(R.id.submit);
         submitButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -62,7 +88,7 @@ public class CameraPage extends AppCompatActivity {
 
                 EditText nameText = (EditText) findViewById(R.id.name);
 
-                String filename = "recipejson";
+                String filename = nameText.toString();
                 JSONObject recipeJSON = new JSONObject();
                 JSONObject individualRecipe = new JSONObject();
                 JSONObject ingredient = new JSONObject();
@@ -84,7 +110,7 @@ public class CameraPage extends AppCompatActivity {
                     individualRecipe.put("name", nameText.toString());
                     individualRecipe.put("ingredients", IngredientList);
                     individualRecipe.put("directions", Directions);
-                    individualRecipe.put("picture", "salt.jpeg");
+                    individualRecipe.put("picture", mCurrentPhotoPath);
                     recipeJSON.put("recipe", individualRecipe);
                 }
                 catch (JSONException e){Log.e("JSON", "JSON not created");}
@@ -100,7 +126,7 @@ public class CameraPage extends AppCompatActivity {
 
                         Log.e("JSON", inputJSON.toString());
                     } catch (Exception e) {
-                        
+
                         e.printStackTrace();
                     }
                 }
@@ -115,7 +141,69 @@ public class CameraPage extends AppCompatActivity {
 
 
 
+
+
     }
+
+
+    //Camera functions
+
+
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageView.setImageBitmap(imageBitmap);
+        }
+    }
+
+
+
+
+
+    //JSON functions
 
     public boolean isExternalStorageWritable() {
         String state = Environment.getExternalStorageState();
